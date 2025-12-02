8bf040e617b71ce28029c7fa3c214181f9940156
uukeex <117409901+uukeex@users.noreply.github.com>
2025-12-02 14:47:41 +0000

    more complicated validation

--- main.tf
+++ main.tf
@@ -3,6 +3,18 @@ variable "server-name" {
   description = "Name of a server to provision"
 }
 
+locals {
+  minNumberOfServers = 1
+  maxNumberOfServers = 10
+}
+variable "number-of-servers" {
+  type = number
+  description = "Required number of servers"
+  validation {
+    condition = var.number-of-servers >= local.minNumberOfServers && var.number-of-servers < local.maxNumberOfServers
+    error_message = "Not supported number of servers, it should be between [${local.minNumberOfServers}, ${local.maxNumberOfServers})"
+  }
+}
 
 output "out" {
   value = var.server-name
