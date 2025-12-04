ce6215c4926cefc80e127dce5ce13fe678b9d607
uukeex <117409901+uukeex@users.noreply.github.com>
2025-12-04 00:27:53 +0000

    simple calc

--- main.tf
+++ main.tf
@@ -1,52 +1,31 @@
-variable "server-name" {
-  type        = string
-  default     = "kot"
-  description = "Name of a server to provision"
-}
+# Write a calculator
 
-locals {
-  minNumberOfServers = 1
-  maxNumberOfServers = 10
-}
-
-locals {
-  countOfItems = {
-    disks   = 13
-    servers = 22
-    max     = local.maxNumberOfServers + var.number-of-disks
-  }
-}
-
-variable "number-of-servers" {
-  type        = number
-  description = "Required number of servers"
-  default     = 2
-  validation {
-    condition     = var.number-of-servers >= local.minNumberOfServers && var.number-of-servers < local.maxNumberOfServers
-    error_message = "Not supported number of servers, it should be between [${local.minNumberOfServers}, ${local.maxNumberOfServers})"
-  }
-}
-
-variable "number-of-disks" {
+variable "a" {
   type = number
 }
 
-variable "list-of-names" {
-  type = list(string)
+variable "z" {
+  type = number
+}
+
+# Allowed: +, -, *, /
+variable "operand" {
+  type = string
+   validation {
+    condition     = contains(["+", "-", "*", "/"], var.operand)
+    error_message = "Invalid operator"
+  }
+}
+
+locals {
+  mapa_kalkulator = {
+    "+" = var.a + var.z
+    "-" = var.a - var.z
+    "*" = var.a * var.z
+    "/" = var.a / var.z
+  }
 }
 
 output "result" {
-  value = "${var.server-name} x ${var.number-of-servers}"
-}
-
-output "number-of-resources" {
-  value = var.number-of-disks + var.number-of-servers
-}
-
-output "list-of-names" {
-  value = join(",", var.list-of-names)
-}
-
-output "key-from-map" {
-  value = lookup(local.countOfItems, "max")
+  value = lookup(local.mapa_kalkulator, var.operand)
 }
