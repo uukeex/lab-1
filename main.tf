0a2aa048cf28d397a7c1abf6920607aa5e2e607f
uukeex <117409901+uukeex@users.noreply.github.com>
2025-12-08 21:18:26 +0000

    hw_połowa

--- main.tf
+++ main.tf
@@ -1,34 +1,64 @@
-variable "a" {
-  type = number
-}
-
-variable "z" {
-  type = number
-
-  validation {
-    condition     = !(var.operand == "/" && var.z == 0)
-    error_message = "Division by zero is not allowed."
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.25.0"
+    }
   }
 }
 
-variable "operand" {
-  type = string
 
-  validation {
-    condition     = contains(["+", "-", "*", "/"], var.operand)
-    error_message = "Invalid operator"
+provider "aws" {
+  default_tags {
+    tags = {
+      Owner = "xx"
+    }
   }
 }
 
-locals {
-  mapa_kalkulator = {
-    "+" = var.a + var.z
-    "-" = var.a - var.z
-    "*" = var.a * var.z
-    "/" = var.a / var.z
+
+variable "bucket_prefix" {
+  description = "S3 bucket prefix"
+  type        = string
+  default     = "awsninja6-"
+}
+
+variable "tags" {
+  description = "tags"
+  type        = map(string)
+  default = {
+    purpose = "learning"
   }
 }
 
-output "result" {
-  value = lookup(local.mapa_kalkulator, var.operand)
+resource "aws_s3_bucket" "my_bucket" {
+  bucket_prefix = var.bucket_prefix
+
+  tags = merge(
+    var.tags,
+    { Owner = "xx" }
+  )
 }
+
+resource "aws_s3_object" "object" {
+  for_each = fileset(path.module, "messages/*")
+  bucket   = aws_s3_bucket.my_bucket.bucket
+  key      = basename(each.key)
+  source   = each.key
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.my_bucket.bucket
+}
+
+output "bucket_arn" {
+  value = aws_s3_bucket.my_bucket.arn
+}
+
+output "bucket_domain_name" {
+  value = aws_s3_bucket.my_bucket.bucket_domain_name
+}
+
+#output "bucket_http_url" {
+#  value = "http://${aws_s3_bucket.my-bucket.bucket}.s3.amazonaws.com"
+#}
